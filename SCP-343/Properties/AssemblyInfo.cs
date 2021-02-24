5ab963e6f585b89edfaad74addfb140ffce483aa
An4r3w <75725607+An4r3w@users.noreply.github.com>
2021-02-24 10:47:31 +0100

    Create AssemblyInfo.cs

---
+++ SCP-343/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyTitle("SCP-343")]
+[assembly: AssemblyDescription("Spawns a magic 343 in gaem!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("An4r3w")]
+[assembly: AssemblyProduct("SCP-343")]
+[assembly: AssemblyCopyright("Copyright © An4r3w 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("53eb7081-7002-42e1-a61f-6207e1113f70")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
